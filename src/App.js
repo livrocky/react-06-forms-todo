4b74fb34874ca7598d1ba03e7016031025495ac5
rokas <rokasra@gmail.com>
2022-06-07 09:00:28 +0300

    template

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1>Heloo again world</h1>
+      <div className='container'>
+        <header>
+          <div className='clear'>
+            <i id='reset' className='fa fa-refresh'></i>
+          </div>
+          <h3 className='done-todo-count'></h3>
+          <p id='date' className='date'>
+            Kraunasi...
+          </p>
+          <img className='img-fluid cover-img' src='img/gold.jpg' alt='Cover image' />
+        </header>
+        <section className='content'>
+          <ul id='list' className='list'></ul>
+          <div className='add-item'>
+            <i id='add-todo-btn' className='fa fa-plus-circle' aria-hidden='true'></i>
+            <input type='text' id='input' placeholder='Add todo' />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
