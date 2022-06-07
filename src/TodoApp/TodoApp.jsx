5e973feac61ada64c661fe8ef8098489632c22b6
rokas <rokasra@gmail.com>
2022-06-07 09:46:53 +0300

    todoList

--- src/TodoApp/TodoApp.jsx
+++ src/TodoApp/TodoApp.jsx
@@ -1,29 +1,11 @@
 import Header from './Header/Header';
+import TodoList from './TodoList/TodoList';
 
 function TodoApp() {
   return (
     <div className='container'>
       <Header />
-      <section className='content'>
-        <ul id='list' className='list'>
-          <li className='item ' data-id='t_2'>
-            <i className='fa fa-circle-thin make-done' aria-hidden='true'></i>
-            <span className='text'>new todo</span>
-            <i className='fa fa-pencil edit-icon' aria-hidden='true'></i>
-            <i className='fa fa-trash delete-icon' aria-hidden='true'></i>
-          </li>
-          <li className='item line-through' data-id='t_2'>
-            <i className='fa fa-check-circle make-done' aria-hidden='true'></i>
-            <span className='text'>done todo</span>
-            <i className='fa fa-pencil edit-icon' aria-hidden='true'></i>
-            <i className='fa fa-trash delete-icon' aria-hidden='true'></i>
-          </li>
-        </ul>
-        <div className='add-item'>
-          <i id='add-todo-btn' className='fa fa-plus-circle' aria-hidden='true'></i>
-          <input type='text' id='input' placeholder='Add todo' />
-        </div>
-      </section>
+      <TodoList />
     </div>
   );
 }
---
+++ src/TodoApp/TodoList/TodoList.jsx
@@ -0,0 +1,26 @@
+function TodoList() {
+  return (
+    <section className='content'>
+      <ul id='list' className='list'>
+        <li className='item ' data-id='t_2'>
+          <i className='fa fa-circle-thin make-done' aria-hidden='true'></i>
+          <span className='text'>new todo</span>
+          <i className='fa fa-pencil edit-icon' aria-hidden='true'></i>
+          <i className='fa fa-trash delete-icon' aria-hidden='true'></i>
+        </li>
+        <li className='item line-through' data-id='t_2'>
+          <i className='fa fa-check-circle make-done' aria-hidden='true'></i>
+          <span className='text'>done todo</span>
+          <i className='fa fa-pencil edit-icon' aria-hidden='true'></i>
+          <i className='fa fa-trash delete-icon' aria-hidden='true'></i>
+        </li>
+      </ul>
+      <div className='add-item'>
+        <i id='add-todo-btn' className='fa fa-plus-circle' aria-hidden='true'></i>
+        <input type='text' id='input' placeholder='Add todo' />
+      </div>
+    </section>
+  );
+}
+
+export default TodoList;
